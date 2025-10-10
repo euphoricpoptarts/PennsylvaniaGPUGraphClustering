ec98bcc0b7222d7141f1d67036b0859c77dbbb9f
Mike Gilbert <euphoricpoptarts@gmail.com>
2025-10-10 15:41:34 -0400

    remove unrelated license; make coarse edge selection more efficient

--- src/leidenR.hpp
+++ src/leidenR.hpp
@@ -1,41 +1,3 @@
-// ***********************************************************************
-// 
-// Jet: Multilevel Graph Partitioning
-//
-// Copyright 2023 National Technology & Engineering Solutions of Sandia, LLC
-// (NTESS). 
-// 
-// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
-// the U.S. Government retains certain rights in this software.
-// 
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-//
-// 1. Redistributions of source code must retain the above copyright
-// notice, this list of conditions and the following disclaimer.
-//
-// 2. Redistributions in binary form must reproduce the above copyright
-// notice, this list of conditions and the following disclaimer in the
-// documentation and/or other materials provided with the distribution.
-//
-// 3. Neither the name of the Corporation nor the names of the
-// contributors may be used to endorse or promote products derived from
-// this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
-// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// ************************************************************************
 #pragma once
 #include <limits>
 #include <random>
@@ -302,7 +264,7 @@ public:
             // randomizations is only strictly necessary on top level graph
             // randomization on successive levels is empirically detrimental to objective quality
             ordinal_t seed = rd();
-            Kokkos::parallel_for("Heaviest HN", policy_t(0, n), KOKKOS_LAMBDA(const ordinal_t i) {
+            Kokkos::parallel_for("select random edge", policy_t(0, n), KOKKOS_LAMBDA(const ordinal_t i) {
                 edge_offset_t end = g.graph.row_map(i + 1);
                 edge_offset_t start = g.graph.row_map(i);
                 ordinal_t width = end - start;
@@ -339,8 +301,43 @@ public:
                 hn(i) = i;
             });
         } else {
-            Kokkos::parallel_for("Heaviest HN", team_policy_t(n, Kokkos::AUTO), KOKKOS_LAMBDA(const member & thread) {
-                ordinal_t i = thread.league_rank();
+            vtx_vt order1 = mem.p_mem.order1;
+            ordinal_t big_begin = mem.p_mem.offset_large;
+            vtx_vt small_vtx = Kokkos::subview(order1, std::make_pair(static_cast<ordinal_t>(0), big_begin));
+            vtx_vt large_vtx = Kokkos::subview(order1, std::make_pair(big_begin, n));
+            Kokkos::parallel_for("select heaviest edge (low degree)", policy_t(0, big_begin), KOKKOS_LAMBDA(const ordinal_t x){
+                ordinal_t i = small_vtx(x);
+                edge_offset_t end = g.graph.row_map(i + 1);
+                edge_offset_t start = g.graph.row_map(i);
+                float multi = gamma*wdeg(i);
+                if(well_conn(i) == 0){
+                    hn(i) = i;
+                    return;
+                }
+                float m = 0;
+                edge_offset_t am = end;
+                // get neighbor maximizing objective
+                for(edge_offset_t j = start; j < end; j++) {
+                    ordinal_t v = g.graph.entries(j);
+                    if(constraint(i) != constraint(v)) continue;
+                    if(well_conn(v) == 0) continue;
+                    scalar_t wgt = g.values(j);
+                    float val = static_cast<float>(wgt) - multi*wdeg(v);
+                    if(val >= m){
+                        m = val;
+                        am = j;
+                    }
+                
+                }
+                if(am >= start && am < end && m >= 0){
+                    ordinal_t h = g.graph.entries(am);
+                    hn(i) = h;
+                } else {
+                    hn(i) = i;
+                }
+            });
+            Kokkos::parallel_for("select heaviest edge (high degree)", team_policy_t(n - big_begin, Kokkos::AUTO), KOKKOS_LAMBDA(const member & thread) {
+                ordinal_t i = large_vtx(thread.league_rank());
                 edge_offset_t end = g.graph.row_map(i + 1);
                 edge_offset_t start = g.graph.row_map(i);
                 float multi = gamma*wdeg(i);
