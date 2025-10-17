aa94d619219c12ac19163013b163c7ffdcb63142
Mike Gilbert <euphoricpoptarts@gmail.com>
2025-10-17 17:10:55 -0400

    making mtx import more robust

--- app/io_mtx.hpp
+++ app/io_mtx.hpp
@@ -5,9 +5,54 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <unordered_set>
 #include "io.hpp"
 
 namespace jet_community {
+
+struct mm_meta {
+    bool is_coordinate;
+    bool is_pattern;
+    bool is_integer;
+    bool is_symmetric;
+};
+
+mm_meta get_mm_metadata(const char* f, const char* fmax){
+    const int max_fields = 5;
+    const std::string PATTERN = "pattern";
+    const std::string INTEGER = "integer";
+    const std::string COORDINATE = "coordinate";
+    const std::string SYMMETRIC = "symmetric";
+    std::string fields[max_fields];
+    int field_num = 0;
+    const char* begin = f;
+    // tracks if we are inside an alphanumeric sequence
+    bool state_machine = false;
+    while(f < fmax) {
+        char c = *f;
+        if(isalnum(c)){
+            if(!state_machine){
+                begin = f;
+            }
+            state_machine = true;
+        } else {
+            if(state_machine){
+                if(field_num < max_fields){
+                    fields[field_num++].assign(begin, f);
+                }
+            }
+            state_machine = false;
+        }
+        f++;
+        if(c == '\n') break;
+    }
+    mm_meta data;
+    data.is_coordinate = (COORDINATE == fields[2]);
+    data.is_pattern = (PATTERN == fields[3]);
+    data.is_integer = (INTEGER == fields[3]);
+    data.is_symmetric = (SYMMETRIC == fields[4]);
+    return data;
+}
 
 bool load_mtx_graph(matrix_t& g, bool& uniform_ew, const char *fname) {
     Kokkos::Timer t;
@@ -30,13 +75,24 @@ bool load_mtx_graph(matrix_t& g, bool& uniform_ew, const char *fname) {
     }
     const char* f = s;
     const char* fmax = s + sz;
-    size_t header[4] = {0, 0, 0, 0};
+    mm_meta metadata = get_mm_metadata(f, fmax);
+    if(!metadata.is_coordinate) {
+        std::cerr << "FATAL ERROR: Importing of non-coordinate format matrix market files is not currently supported!" << std::endl;
+        return false;
+    }
+    if(!metadata.is_pattern) {
+        std::cout << "WARNING: Edge weights given in matrix market file. This is not currently supported. The given weights will be ignored." << std::endl;
+    }
+    if(!metadata.is_symmetric) {
+        std::cout << "INFO: Non-symmetric matrix market file detected. The graph will be automatically symmetrized." << std::endl;
+    }
+    size_t sub_header[4] = {0, 0, 0, 0};
     //ignore commented lines
     while(*f == '%') next_line(f);
     while(!isdigit(*f)) f++;
     //read header data
     for(int i = 0; i < 4; i++){
-        header[i] = fast_atoi<size_t>(f);
+        sub_header[i] = fast_atoi<size_t>(f);
         while(!isdigit(*f)){
             if(*f == '\n'){
                 //end for loop
@@ -48,24 +104,18 @@ bool load_mtx_graph(matrix_t& g, bool& uniform_ew, const char *fname) {
         }
     }
     bool has_ew = false;
-    ordinal_t n = header[0];
-    edge_offset_t m = header[2];
+    ordinal_t n = sub_header[0];
+    if(sub_header[0] != sub_header[1]){
+        std::cerr << "FATAL ERROR: Bipartite file format detected. This format is not currently supported!" << std::endl;
+        return false;
+    }
+    edge_offset_t m = sub_header[2];
     std::vector<ordinal_t> src(m);
     std::vector<ordinal_t> dst(m);
 
-    vtx_view_t entries(Kokkos::ViewAllocateWithoutInitializing("entries"), m*2);
-    vtx_mirror_t entries_m = Kokkos::create_mirror_view(entries);
-    edge_view_t row_map(Kokkos::ViewAllocateWithoutInitializing("row_map"), n + 1);
-    edge_mirror_t row_map_m = Kokkos::create_mirror_view(row_map);
-    Kokkos::deep_copy(row_map_m, 0);
-    wgt_view_t values(Kokkos::ViewAllocateWithoutInitializing("values"), 2*m);
-    wgt_mirror_t values_m;
-    if(has_ew){
-        values_m = Kokkos::create_mirror_view(values);
-    }
     edge_offset_t edges_read = 0;
     bool is_dest = false;
-    //ready edge information
+    //read edge information
     while(f < fmax){
         //increment past whitespace
         while(f < fmax && !isdigit(*f)){
@@ -87,7 +137,6 @@ bool load_mtx_graph(matrix_t& g, bool& uniform_ew, const char *fname) {
         ordinal_t v = fast_atoi<ordinal_t>(f);
         //subtract 1 to convert to 0-indexed
         v--;
-        row_map_m(v)++;
         if(!is_dest){
             src[edges_read] = v;
             is_dest = true;
@@ -99,21 +148,72 @@ bool load_mtx_graph(matrix_t& g, bool& uniform_ew, const char *fname) {
     }
     delete[] s;
     if(edges_read != m){
-        std::cerr << "FATAL ERROR: Mismatch between expected and actual line/nonzero count in metis file" << std::endl;
+        std::cerr << "FATAL ERROR: Mismatch between expected and actual line/nonzero count in matrix market file" << std::endl;
         std::cerr << "Read " << edges_read << " nonzeros" << std::endl;
         std::cerr << "Nonzeros expected: " << m << std::endl;
         return false;
     }
+
+    int self_loops = 0;
+    edge_view_t row_map(Kokkos::ViewAllocateWithoutInitializing("row_map"), n + 1);
+    edge_mirror_t row_map_m = Kokkos::create_mirror_view(row_map);
+    Kokkos::deep_copy(row_map_m, 0);
+    // unique_edges should be deleted after this block
+    {
+        // count edges per vertex
+        std::unordered_set<uint64_t> unique_edges;
+        for(edge_offset_t j = 0; j < edges_read; j++){
+            ordinal_t u = src[j];
+            ordinal_t v = dst[j];
+            if(u == v){
+                self_loops++;
+                continue;
+            }
+            if(!metadata.is_symmetric){
+                uint64_t less = (u < v) ? u : v;
+                uint64_t more = (u > v) ? u : v;
+                uint64_t signature = less*n + more;
+                if(unique_edges.count(signature) != 0){
+                    // ignore this edge in a later loop
+                    src[j] = dst[j];
+                    continue;
+                }
+                unique_edges.insert(signature);
+            }
+            row_map_m(u)++;
+            row_map_m(v)++;
+        }
+    }
+    if(self_loops > 0){
+        std::cout << "WARNING: Ignoring " << self_loops << " self loop edges." << std::endl;
+    }
+    // if we don't clear this here
+    // then it sometimes (somehow) affects the timing of the clustering algorithm
+    // unique_edges.clear();
     edge_offset_t sum = 0;
+    // compute row map
     for(ordinal_t i = 0; i <= n; i++){
         ordinal_t d = row_map_m(i);
         row_map_m(i) = sum;
         sum += d;
     }
+
+    // must adjust for replicated edges and self loops that we ignored
+    m = sum / 2;
+    vtx_view_t entries(Kokkos::ViewAllocateWithoutInitializing("entries"), m*2);
+    vtx_mirror_t entries_m = Kokkos::create_mirror_view(entries);
+    wgt_view_t values(Kokkos::ViewAllocateWithoutInitializing("values"), 2*m);
+    wgt_mirror_t values_m;
+    if(has_ew){
+        values_m = Kokkos::create_mirror_view(values);
+    }
+
     std::vector<ordinal_t> counters(n, 0);
+    // write edges to entries
     for(edge_offset_t j = 0; j < edges_read; j++){
         ordinal_t u = src[j];
         ordinal_t v = dst[j];
+        if(u == v) continue;
         edge_offset_t u_offset = row_map_m(u) + counters[u]++;
         edge_offset_t v_offset = row_map_m(v) + counters[v]++;
         entries_m(u_offset) = v;
@@ -128,6 +228,7 @@ bool load_mtx_graph(matrix_t& g, bool& uniform_ew, const char *fname) {
         Kokkos::deep_copy(values, 1);
         uniform_ew = true;
     }
+
     graph_t g_graph(entries, row_map);
     g = matrix_t("input graph", n, values, g_graph);
     std::cout << "Read graph from " << fname << " in " << std::setprecision(3) << t.seconds() << "s" << std::endl;
