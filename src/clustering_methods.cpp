euphoricpoptarts/PennsylvaniaGPUGraphClustering

commit cc87c838db533a9ab537b0c751c53c231d4d440f

eliminate is_initial parameter from local_move functions, compute it instead

src/clustering_methods.cpp
@@ -61,13 +61,13 @@ namespace clustering_methods {
             double old_obj = rfd.obj;
             // orderings must be generated for use in local_move and build_coarse_graph
             order::generate_orderings(mem, c.mtx);
-            lm_t::local_move<false>(c, part, rfd, !improve && (levels.size() == 1), mem, part, true);
+            lm_t::local_move<false>(c, part, rfd, mem, part, true);
             if(c.mtx.nnz() > 100000 && old_obj == rfd.obj){
                 // it is usually worth trying this first
-                lm_t::local_move<false>(c, part, rfd, !improve && (levels.size() == 1), mem, part, false);
+                lm_t::local_move<false>(c, part, rfd, mem, part, false);
             }
             if(old_obj == rfd.obj){
-                lm_t::local_move_strict<false>(c, part, rfd, !improve && (levels.size() == 1), mem, part);
+                lm_t::local_move_strict<false>(c, part, rfd, mem, part);
             }
             if(rfd.label_count == c.mtx.numRows()){
                 parts.push_back(part);
@@ -130,7 +130,7 @@ namespace clustering_methods {
             });
             if constexpr(plus){
                 order::generate_orderings(mem, c.mtx);
-                lm_t::local_move<false>(c, fine_part, rfd, false, mem, part, true);
+                lm_t::local_move<false>(c, fine_part, rfd, mem, part, true);
             }
         }
         return parts[0];
@@ -152,11 +152,12 @@ namespace clustering_methods {
             });
             // orderings must be generated for use in local_move and build_coarse_graph
             order::generate_orderings(mem, c.mtx);
-            lm_t::local_move<constrained>(c, part, rfd, true, mem, constraint, true);
+            lm_t::local_move<constrained>(c, part, rfd, mem, constraint, true);
             // the user clearly cares about quality if they are doing multiple iterations
             if(constrained && rfd.label_count == c.mtx.numRows()){
-                lm_t::local_move_strict<constrained>(c, part, rfd, true, mem, constraint);
+                lm_t::local_move_strict<constrained>(c, part, rfd, mem, constraint);
             }
+            // this logic (when part is appended to vector) should be reworked
             parts.push_back(part);
             if(rfd.label_count < c.mtx.numRows()){
 #ifdef EXTRA_TIMING
@@ -183,6 +184,7 @@ namespace clustering_methods {
 #endif
             } else if(drop_constraint) {
                 Kokkos::deep_copy(constraint, 0);
+                // this logic should be reworked
                 parts.pop_back();
                 drop_constraint = false;
             } else {
@@ -214,7 +216,7 @@ namespace clustering_methods {
                 part(x) = coarse_part(part(x));
             });
             order::generate_orderings(mem, c.mtx);
-            lm_t::local_move<false>(c, part, rfd, false, mem, constraint, true);
+            lm_t::local_move<false>(c, part, rfd, mem, constraint, true);
         }
 
         experiment.addMeasurement(Measurement::Contract, aggregate);

src/local_mover.cpp
@@ -1371,7 +1371,7 @@ void clone_pval(mem_t& mem, ordinal_t n){
 
 // moves vertices between clusters such that the objective increases
 template <bool constrained>
-void local_move(const wg_t wg, vtx_vt best_part, refine_data& best_state, bool is_initial, mem_t& mem, vtx_vt constraint, bool enable_simulated_annealing){
+void local_move(const wg_t wg, vtx_vt best_part, refine_data& best_state, mem_t& mem, vtx_vt constraint, bool enable_simulated_annealing){
     const matrix_t g = wg.mtx;
     // this is a reference to avoid allocating new memory
     refine_data& curr_state = mem.spare_cluster_data;
@@ -1379,6 +1379,9 @@ void local_move(const wg_t wg, vtx_vt best_part, refine_data& best_state, bool i
     vtx_vt part = Kokkos::subview(mem.p_mem.part, std::make_pair(static_cast<ordinal_t>(0), g.numRows()));
     Kokkos::deep_copy(exec_space(), part, best_part);
     cdata_t cdata = truncate_and_init_mem(mem, wg, best_state.label_count, best_state.top_nnz == g.nnz());
+    // enable certain optimizations when the clustering consists only of singleton clusters
+    // this further requires that each vertex v is in cluster v (which should always be the case in this software for singleton clusterings)
+    bool is_initial = (best_state.label_count == g.numRows());
     if(!is_initial){
         if(wg.edge_uniform) init_conn_graph<true>(wg, part, cdata, mem);
         else init_conn_graph<false>(wg, part, cdata, mem);
@@ -1432,7 +1435,7 @@ void local_move(const wg_t wg, vtx_vt best_part, refine_data& best_state, bool i
 // with some exceptions due to floating-point roundoff
 // this occurs specifically if a vertex is close to node-optimal, having a near-zero objective delta to a neighboring cluster 
 template <bool constrained>
-void local_move_strict(const wg_t wg, vtx_vt best_part, refine_data& best_state, bool is_initial, mem_t& mem, vtx_vt constraint){
+void local_move_strict(const wg_t wg, vtx_vt best_part, refine_data& best_state, mem_t& mem, vtx_vt constraint){
     const matrix_t g = wg.mtx;
     // this is a reference to avoid allocating new memory
     refine_data& curr_state = mem.spare_cluster_data;
@@ -1440,6 +1443,9 @@ void local_move_strict(const wg_t wg, vtx_vt best_part, refine_data& best_state,
     vtx_vt part = Kokkos::subview(mem.p_mem.part, std::make_pair(static_cast<ordinal_t>(0), g.numRows()));
     Kokkos::deep_copy(exec_space(), part, best_part);
     cdata_t cdata = truncate_and_init_mem(mem, wg, best_state.label_count, best_state.top_nnz == g.nnz());
+    // enable certain optimizations when the clustering consists only of singleton clusters
+    // this further requires that each vertex v is in cluster v (which should always be the case in this software for singleton clusterings)
+    bool is_initial = (best_state.label_count == g.numRows());
     if(!is_initial){
         if(wg.edge_uniform) init_conn_graph<true>(wg, part, cdata, mem);
         else init_conn_graph<false>(wg, part, cdata, mem);
@@ -1495,11 +1501,11 @@ void local_move_strict(const wg_t wg, vtx_vt best_part, refine_data& best_state,
 }
 
     // explicit template instantiations
-    template void local_move<true>(const wg_t wg, vtx_vt best_part, refine_data& best_state, bool is_initial, mem_t& mem, vtx_vt constraint, bool enable_simulated_annealing);
-    template void local_move<false>(const wg_t wg, vtx_vt best_part, refine_data& best_state, bool is_initial, mem_t& mem, vtx_vt constraint, bool enable_simulated_annealing);
+    template void local_move<true>(const wg_t wg, vtx_vt best_part, refine_data& best_state, mem_t& mem, vtx_vt constraint, bool enable_simulated_annealing);
+    template void local_move<false>(const wg_t wg, vtx_vt best_part, refine_data& best_state, mem_t& mem, vtx_vt constraint, bool enable_simulated_annealing);
 
-    template void local_move_strict<true>(const wg_t wg, vtx_vt best_part, refine_data& best_state, bool is_initial, mem_t& mem, vtx_vt constraint);
-    template void local_move_strict<false>(const wg_t wg, vtx_vt best_part, refine_data& best_state, bool is_initial, mem_t& mem, vtx_vt constraint);
+    template void local_move_strict<true>(const wg_t wg, vtx_vt best_part, refine_data& best_state, mem_t& mem, vtx_vt constraint);
+    template void local_move_strict<false>(const wg_t wg, vtx_vt best_part, refine_data& best_state, mem_t& mem, vtx_vt constraint);
 
 }
 

interface/memory_store.hpp
@@ -33,7 +33,6 @@ struct memory_store {
         obj_vt obj_persistent;
         wgt_vt pvals, pvals_clone;
         vtx_vt part, dest_part;
-        vtx_vt dest_cache;
 
         persistent(const matrix_t largest){
             ordinal_t n = largest.numRows();

src/header/local_mover.h
@@ -19,10 +19,10 @@ namespace local_move_heuristic {
     using wg_t = weighted_graph;
 
     template <bool constrained>
-    void local_move(const wg_t wg, vtx_vt best_part, refine_data& best_state, bool is_initial, mem_t& mem, vtx_vt constraint, bool enable_simulated_annealing);
+    void local_move(const wg_t wg, vtx_vt best_part, refine_data& best_state, mem_t& mem, vtx_vt constraint, bool enable_simulated_annealing);
 
     template <bool constrained>
-    void local_move_strict(const wg_t wg, vtx_vt best_part, refine_data& best_state, bool is_initial, mem_t& mem, vtx_vt constraint);
+    void local_move_strict(const wg_t wg, vtx_vt best_part, refine_data& best_state, mem_t& mem, vtx_vt constraint);
 
 }
 

commit 92303f1cd4c5ac2c63efc58fa8498f0869040a5b

reduce code duplication; fix bug introduced in commit cc87c83

src/clustering_methods.cpp
@@ -44,20 +44,35 @@ namespace clustering_methods {
         });
     }
 
+    void upsample(vtx_vt in, vtx_vt out, ordinal_t n){
+        Kokkos::parallel_for("upsample cluster assignments", policy_t(0, n), KOKKOS_LAMBDA(const ordinal_t x){
+            out(x) = in(out(x));
+        });
+    }
+
+    void set_singleton(vtx_vt part, ordinal_t n){
+        Kokkos::parallel_for("set as singleton clustering", policy_t(0, n), KOKKOS_LAMBDA(const ordinal_t i){
+            part(i) = i;
+        });
+    }
+
     template <bool plus, bool improve>
     vtx_vt leiden_part(mem_t& mem, wg_t top, rfd_t& rfd, ExperimentLoggerUtil<value_t>& experiment, vtx_vt input){
         std::vector<wg_t> levels;
         std::vector<vtx_vt> parts;
         levels.push_back(top);
         double aggregate = 0;
-        vtx_vt part("cluster assignments", top.mtx.numRows());
-        if(!improve){
-            Kokkos::parallel_for("set initial assignments", policy_t(0, top.mtx.numRows()), KOKKOS_LAMBDA(const ordinal_t x){
-                part(x) = x;
-            });
-        } else Kokkos::deep_copy(part, input);
+        vtx_vt part(Kokkos::ViewAllocateWithoutInitializing("cluster assignments"), top.mtx.numRows());
+        if(improve) Kokkos::deep_copy(part, input);
         while(true) {
             wg_t c = levels[levels.size() - 1];
+            if(rfd.label_count == c.mtx.numRows()){
+                // ensure that any singleton clustering has each vertex v assigned to cluster v
+                set_singleton(part, rfd.label_count);
+                wgt_vt td_rfd = Kokkos::subview(rfd.total_deg, std::make_pair((ordinal_t)0, rfd.label_count));
+                // ensure that the cluster weights are correct
+                Kokkos::deep_copy(td_rfd, c.vtx_w);
+            }
             double old_obj = rfd.obj;
             // orderings must be generated for use in local_move and build_coarse_graph
             order::generate_orderings(mem, c.mtx);
@@ -69,7 +84,8 @@ namespace clustering_methods {
             if(old_obj == rfd.obj){
                 lm_t::local_move_strict<false>(c, part, rfd, mem, part);
             }
-            if(rfd.label_count == c.mtx.numRows()){
+            // every vertex in a singleton cluster and no improvements possible
+            if(old_obj == rfd.obj && rfd.label_count == c.mtx.numRows()){
                 parts.push_back(part);
                 break;
             }
@@ -95,10 +111,11 @@ namespace clustering_methods {
                 wg_t next_level;
                 if(c.edge_uniform) next_level = contract_t::build_coarse_graph<true, false>(c, coarse_map, coarse_vtx_count, mem);
                 else next_level = contract_t::build_coarse_graph<false, false>(c, coarse_map, coarse_vtx_count, mem);
+                // this must be zero-initialized
                 next_level.vtx_w = wgt_vt("weighted degree 2", coarse_vtx_count);
                 coarsen_vtx_w(c.vtx_w, next_level.vtx_w, coarse_map);
 
-                part = vtx_vt("cluster assignments coarse", coarse_vtx_count);
+                part = vtx_vt(Kokkos::ViewAllocateWithoutInitializing("cluster assignments coarse"), coarse_vtx_count);
                 downsample(louv, part, coarse_map);
                 levels.push_back(next_level);
 
@@ -125,9 +142,7 @@ namespace clustering_methods {
             wg_t c = levels[i];
             vtx_vt coarse_part = parts[i + 1];
             vtx_vt fine_part = parts[i];
-            Kokkos::parallel_for("update top level assignments", policy_t(0, c.mtx.numRows()), KOKKOS_LAMBDA(const ordinal_t x){
-                fine_part(x) = coarse_part(fine_part(x));
-            });
+            upsample(coarse_part, fine_part, c.mtx.numRows());
             if constexpr(plus){
                 order::generate_orderings(mem, c.mtx);
                 lm_t::local_move<false>(c, fine_part, rfd, mem, part, true);
@@ -146,10 +161,8 @@ namespace clustering_methods {
         bool drop_constraint = constrained;
         while(true) {
             wg_t c = levels[levels.size() - 1];
-            vtx_vt part("cluster assignments", c.mtx.numRows());
-            Kokkos::parallel_for("set initial assignments", policy_t(0, c.mtx.numRows()), KOKKOS_LAMBDA(const ordinal_t x){
-                part(x) = x;
-            });
+            vtx_vt part(Kokkos::ViewAllocateWithoutInitializing("cluster assignments"), c.mtx.numRows());
+            set_singleton(part, c.mtx.numRows());
             // orderings must be generated for use in local_move and build_coarse_graph
             order::generate_orderings(mem, c.mtx);
             lm_t::local_move<constrained>(c, part, rfd, mem, constraint, true);
@@ -168,12 +181,12 @@ namespace clustering_methods {
                 if(c.edge_uniform) next_level = contract_t::build_coarse_graph<true, true>(c, part, rfd.label_count, mem);
                 else next_level = contract_t::build_coarse_graph<false, false>(c, part, rfd.label_count, mem);
                 wgt_vt td_rfd = Kokkos::subview(rfd.total_deg, std::make_pair((ordinal_t)0, rfd.label_count));
-                next_level.vtx_w = wgt_vt("next level vtx weights", rfd.label_count);
+                next_level.vtx_w = wgt_vt(Kokkos::ViewAllocateWithoutInitializing("next level vtx weights"), rfd.label_count);
                 Kokkos::deep_copy(next_level.vtx_w, td_rfd);
                 levels.push_back(next_level);
 
                 if(constrained) {
-                    vtx_vt next_constraint("next constraint", rfd.label_count);
+                    vtx_vt next_constraint(Kokkos::ViewAllocateWithoutInitializing("next constraint"), rfd.label_count);
                     downsample(constraint, next_constraint, part);
                     constraint = next_constraint;
                 }
@@ -212,9 +225,7 @@ namespace clustering_methods {
             wg_t c = levels[i];
             vtx_vt coarse_part = parts[i + 1];
             vtx_vt part = parts[i];
-            Kokkos::parallel_for("update top level assignments", policy_t(0, c.mtx.numRows()), KOKKOS_LAMBDA(const ordinal_t x){
-                part(x) = coarse_part(part(x));
-            });
+            upsample(coarse_part, part, c.mtx.numRows());
             order::generate_orderings(mem, c.mtx);
             lm_t::local_move<false>(c, part, rfd, mem, constraint, true);
         }

src/local_mover.cpp
@@ -1352,7 +1352,7 @@ void local_move(const wg_t wg, vtx_vt best_part, refine_data& best_state, mem_t&
     Kokkos::deep_copy(exec_space(), part, best_part);
     cdata_t cdata = truncate_and_init_mem(mem, wg, best_state.label_count, best_state.top_nnz == g.nnz());
     // enable certain optimizations when the clustering consists only of singleton clusters
-    // this further requires that each vertex v is in cluster v (which should always be the case in this software for singleton clusterings)
+    // this further requires that each vertex v is in cluster v (which takes some effort to ensure for Leiden)
     bool is_initial = (best_state.label_count == g.numRows());
     if(!is_initial){
         if(wg.edge_uniform) init_conn_graph<true>(wg, part, cdata, mem);
@@ -1416,12 +1416,11 @@ void local_move_strict(const wg_t wg, vtx_vt best_part, refine_data& best_state,
     Kokkos::deep_copy(exec_space(), part, best_part);
     cdata_t cdata = truncate_and_init_mem(mem, wg, best_state.label_count, best_state.top_nnz == g.nnz());
     // enable certain optimizations when the clustering consists only of singleton clusters
-    // this further requires that each vertex v is in cluster v (which should always be the case in this software for singleton clusterings)
+    // this further requires that each vertex v is in cluster v (which takes some effort to ensure for Leiden)
     bool is_initial = (best_state.label_count == g.numRows());
     if(!is_initial){
         if(wg.edge_uniform) init_conn_graph<true>(wg, part, cdata, mem);
         else init_conn_graph<false>(wg, part, cdata, mem);
-        clone_pval(mem, g.numRows());
         curr_state.last_pval = pval_sum(mem.p_mem.pvals, g.numRows());
     } else {
         curr_state.last_pval = 0;